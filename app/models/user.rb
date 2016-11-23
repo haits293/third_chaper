2958a28fa421ae4c47756020c2b9c64392c03659
Hai Tran <haits293@gmail.com>
2016-11-23 11:29:33 +0700

    Implement basic login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   before_save :downcase_email
-  
+
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: 255},
@@ -10,8 +10,16 @@ class User < ApplicationRecord
   validates :password, presence: {allow_nil: true},
     length: {minimum: 6}
   validates :date_of_birth, presence: true
-  
+
   def downcase_email
     self.email.downcase!
+  end
+
+  class << self
+    def digest string
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+        BCrypt::Engine.cost
+      BCrypt::Password.create string, cost: cost
+    end
   end
 end
