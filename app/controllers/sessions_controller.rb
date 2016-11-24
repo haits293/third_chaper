988115fb1abdc23e474a265a7024803838650205
Hai Tran <haits293@gmail.com>
2016-11-24 15:59:55 +0700

    Add account activation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,20 +4,25 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by email: params[:session][:email]
-    if user.active?
-      if user && user.authenticate(params[:session][:password])
+    if user && user.authenticate(params[:session][:password])
+      if user.active?
         log_in user
         params[:session][:remember_me] == "1" ? remember(user) : forget(user)
         flash[:success] = "Hello #{user.name}"
         redirect_back_or user
+      elsif user.inactive?
+        message  = "Account not activated.
+          Check your email for the activation link."
+        flash[:warning] = message
+        redirect_to root_url
       else
-        flash[:danger] = "Invalid email/password combination"
-        render :new
+        flash[:danger] = "This account has been suspended.
+          Please contact the administrator. Thank you."
+        redirect_to login_path
       end
     else
-      flash[:danger] = "This account has been suspended.
-        Please contact the administrator. Thank you."
-      redirect_to login_path
+      flash[:danger] = "Invalid email/password combination"
+      render :new
     end
   end
 
--- db/migrate/20161123101602_add_status_to_users.rb
+++ db/migrate/20161123101602_add_status_to_users.rb
@@ -1,5 +1,5 @@
 class AddStatusToUsers < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :status, :integer, default: 0
+    add_column :users, :status, :integer, default: 2
   end
 end
